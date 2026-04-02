a31c006487964363a5e539d92726815efa44746a
rudneigaspar <72238189+rudneigaspar@users.noreply.github.com>
2026-04-02 17:39:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ st.subheader("Acompanhe o crescimento dos participantes no Instagram")
 # Chave: Nome exibido no site | Valor: @ do Instagram (sem o @)
 participantes = {
     "👑 Líder": "nome_do_lider",
-    "🔥 No Paredão": "nome_emparedado_1",
-    "🔥 No Paredão 2": "nome_emparedado_2",
+    #"🔥 No Paredão": "nome_emparedado_1",
+    #"🔥 No Paredão 2": "nome_emparedado_2",
     "Participante 4": "usuario_4",
     "Participante 5": "usuario_5",
     "Participante 6": "usuario_6",
